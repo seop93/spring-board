03b34f2db27a7a63a0fb69f252595c7eff00e8fd
byeonheungseob <kevin9160@naver.com>
2022-11-10 16:30:43 +0900

    도커파일

--- src/test/java/com/mustache/bb3/Bb3ApplicationTests.java
+++ src/test/java/com/mustache/bb3/Bb3ApplicationTests.java
@@ -3,11 +3,12 @@ package com.mustache.bb3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class Bb3ApplicationTests {
-//
+@SpringBootTest
+class Bb3ApplicationTests {
+
 //    @Test
 //    void contextLoads() {
 //    }
-//
-//}
+
+}
+
