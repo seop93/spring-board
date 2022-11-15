946c75ea577316befabe428d30b982b3f7940446
byeonheungseob <kevin9160@naver.com>
2022-11-15 14:24:24 +0900

    리포지토리 -> 서비스 -> RestController

--- src/main/java/com/mustache/bb3/controller/HospitalRestController.java
+++ src/main/java/com/mustache/bb3/controller/HospitalRestController.java
@@ -3,6 +3,7 @@ package com.mustache.bb3.controller;
 import com.mustache.bb3.domain.dto.HospitalResponse;
 import com.mustache.bb3.domain.entity.Hospital;
 import com.mustache.bb3.repository.HospitalRepository;
+import com.mustache.bb3.service.HospitalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,16 +15,15 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/hospitals")
 public class HospitalRestController {
-    private final HospitalRepository hospitalRepository;
+    private final HospitalService hospitalService;
 
-    public HospitalRestController(HospitalRepository hospitalRepository) {
-        this.hospitalRepository = hospitalRepository;
+    public HospitalRestController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) { //ResponseEnitty
-        Optional<Hospital> hospital = hospitalRepository.findById(id); // Entity
-        HospitalResponse hospitalResponse = Hospital.of(hospital.get()); //Response == DTO
-        return ResponseEntity.ok().body(hospitalResponse);
+    public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) { // ResponseEntity도 DTO타입
+        HospitalResponse hospitalResponse = hospitalService.getHospital(id); // DTO
+        return ResponseEntity.ok().body(hospitalResponse); // Return은 DTO로
     }
 }
--- src/main/java/com/mustache/bb3/domain/dto/HospitalResponse.java
+++ src/main/java/com/mustache/bb3/domain/dto/HospitalResponse.java
@@ -1,18 +1,15 @@
 package com.mustache.bb3.domain.dto;
 
 import com.mustache.bb3.domain.entity.Hospital;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Builder
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class HospitalResponse {
-
     private Integer id;
     private String roadNameAddress;
     private String hospitalName;
@@ -20,6 +17,20 @@ public class HospitalResponse {
     private Integer totalNumberOfBeds;
     private String businessTypeName;
     private Float totalAreaSize;
+    public String businessStatusName;
+    public Integer businessStatusCode;
 
+    public HospitalResponse(Integer id, String roadNameAddress, String hospitalName, Integer patientRoomCount, Integer totalNumberOfBeds, String businessTypeName, Float totalAreaSize) {
+        this.id = id;
+        this.roadNameAddress = roadNameAddress;
+        this.hospitalName = hospitalName;
+        this.patientRoomCount = patientRoomCount;
+        this.totalNumberOfBeds = totalNumberOfBeds;
+        this.businessTypeName = businessTypeName;
+        this.totalAreaSize = totalAreaSize;
+    }
 
+    public void setBusinessStatusName(String businessStatusName) {
+        this.businessStatusName = businessStatusName;
+    }
 }
