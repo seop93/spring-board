316c175c6b60ec970331005102d28b551292f7d4
byeonheungseob <kevin9160@naver.com>
2022-11-15 14:23:38 +0900

    서비스 추가

---
+++ src/main/java/com/mustache/bb3/service/HospitalService.java
@@ -0,0 +1,33 @@
+package com.mustache.bb3.service;
+
+import com.mustache.bb3.domain.dto.HospitalResponse;
+import com.mustache.bb3.domain.entity.Hospital;
+import com.mustache.bb3.repository.HospitalRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HospitalService {
+
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    public HospitalResponse getHospital(Integer id) {
+        Optional<Hospital> optHospital = hospitalRepository.findById(id); // Entity
+        Hospital hospital = optHospital.get();
+        HospitalResponse hospitalResponse = Hospital.of(hospital); // DTO
+        if (hospital.getBusinessStatusCode() == 13) {
+            hospitalResponse.setBusinessStatusName("영업중");
+        } else if (hospital.getBusinessStatusCode() == 3) {
+            hospitalResponse.setBusinessStatusName("폐업");
+        } else {
+            hospitalResponse.setBusinessStatusName(String.valueOf(hospital.getBusinessStatusCode()));
+        }
+        return hospitalResponse;
+    }
+
+}
