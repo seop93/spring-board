341aa00a17de91ca78e08eb8f99f38687ca16cc3
byeonheungseob <kevin9160@naver.com>
2022-11-15 14:24:34 +0900

    수정

--- src/main/java/com/mustache/bb3/controller/HospitalController.java
+++ src/main/java/com/mustache/bb3/controller/HospitalController.java
@@ -1,13 +1,17 @@
 package com.mustache.bb3.controller;
 
+import com.mustache.bb3.domain.dto.HospitalResponse;
 import com.mustache.bb3.domain.entity.Hospital;
 import com.mustache.bb3.repository.HospitalRepository;
+import com.mustache.bb3.service.HospitalService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -17,18 +21,18 @@ public class HospitalController {
 
     private final HospitalRepository hospitalRepository;
 
-    public HospitalController(HospitalRepository hospitalRepository){
+    public HospitalController(HospitalRepository hospitalRepository) {
         this.hospitalRepository = hospitalRepository;
     }
 
-    @GetMapping("")
-    public String list(Pageable pageable, Model model){
-        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
 
+    @GetMapping("")
+    public String hospitalList(Model model, Pageable pageable) {
+        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
         log.info("size:{}", hospitals.getSize());
         model.addAttribute("hospitals", hospitals);
         model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
         model.addAttribute("next", pageable.next().getPageNumber());
-        return "hospitals/list";
+        return "hospital/list";
     }
 }
--- src/main/java/com/mustache/bb3/domain/entity/Hospital.java
+++ src/main/java/com/mustache/bb3/domain/entity/Hospital.java
@@ -5,6 +5,7 @@ import com.mustache.bb3.domain.dto.HospitalResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,19 +15,25 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "nation_wide_hospitals") //hospital 이라는 테이블이 아님
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hospital {
     @Id
     private Integer id;
 
-    @Column(name = "road_name_address")
-    private String roadNameAddress;
-
     @Column(name = "hospital_name")
     private String hospitalName;
+
+    @Column(name="road_name_address")
+    private String roadNameAddress;
+
     private Integer patientRoomCount;
     private Integer totalNumberOfBeds;
     private String businessTypeName;
     private Float totalAreaSize;
+    public String businessStatusName;
+    public Integer businessStatusCode;
 
     // HospitalEntity를 HospitalResponse Dto로 만들어주는 부분
     public static HospitalResponse of(Hospital hospital) {
